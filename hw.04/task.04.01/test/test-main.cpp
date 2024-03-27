b074944f61fe0d7c1c62f00dbbdc03caaaccf1a0
ZuMi <zubetzmikhael@gmail.com>
2024-03-27 20:43:58 +0300

    added task.04.01 solution

--- hw.04/task.04.01/test/test-main.cpp
+++ hw.04/task.04.01/test/test-main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 #include "LinkedList.h"
 
-int main(){
-    std::cout << "Hello List!" << std::endl;
-    return 0;
+#include "Catch2/catch_test_macros.hpp"
+#include "Catch2/catch_session.hpp"
+
+TEST_CASE("Testing LinkedList", "[list]")
+{
+    List list;
+    SECTION("Size") {
+        CHECK(list.Size() == 0);
+        list.PushFront(1);
+        list.PushFront(0);
+        list.PushBack(2);
+        CHECK(list.Size() == 3);
+    }
+    SECTION("Empty") {
+        list.Size() == 0;
+        CHECK(list.Empty() == true);
+        list.PushFront(1);
+        list.PushBack(2);
+        CHECK(list.Empty() == false);
+    }
+    SECTION("Clear") {
+        list.Clear();
+        CHECK(list.Size() == 0);
+    }
+}
+
+int main()
+{
+    return Catch::Session().run();
 }
